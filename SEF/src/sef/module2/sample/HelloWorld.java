849cb8605e44bf3c0cf252e5c2e6ba37a7a8797f
Atif <hafiz.mahmood@accenture.com>
2020-10-19 17:18:35 +0300

    Day1- Activites

--- SEF/src/sef/module2/sample/HelloWorld.java
+++ SEF/src/sef/module2/sample/HelloWorld.java
@@ -2,7 +2,18 @@ package sef.module2.sample;
 
 public class HelloWorld {
 	
+	public static String insVar1;
+	//public static String insVar1;
+	
 	public static void main(String arg[]){
-		System.out.println("Hello World!");
+		insVar1="";
+		String abc = "hay guys";
+		abc = "xyz";
+		System.out.println(abc);
+	}
+	
+	public static void main1(String temp){
+		System.out.println(temp);
 	}
 }
+
