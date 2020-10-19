47fb689708d6e61b5f37b1266328b479f29c91fc
Atif <hafiz.mahmood@accenture.com>
2020-10-19 17:21:41 +0300

    Activity for Day-1

--- SEF/src/sef/module2/sample/HelloWorld.java
+++ SEF/src/sef/module2/sample/HelloWorld.java
@@ -2,18 +2,11 @@ package sef.module2.sample;
 
 public class HelloWorld {
 	
-	public static String insVar1;
-	//public static String insVar1;
-	
 	public static void main(String arg[]){
-		insVar1="";
-		String abc = "hay guys";
-		abc = "xyz";
-		System.out.println(abc);
+		System.out.println("Hello World!");
 	}
 	
-	public static void main1(String temp){
-		System.out.println(temp);
+	public static void main1(String arg[]){
+		System.out.println("Hello World!");
 	}
 }
-
--- SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -2,24 +2,17 @@
  * 
  */
 package sef.module3.activity;
-import sef.module2.*;
-import sef.module2.sample.HelloWorld;
 
 /**
  * @author 
  *
  */
 public class VariableAssignmentActivity {
-	static HelloWorld hw = new HelloWorld();
-	static HelloWorld hw1 = new HelloWorld();
-	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		hw.main1("This is our First Name");
-		hw1.main1("This is our second Name");
 		
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
