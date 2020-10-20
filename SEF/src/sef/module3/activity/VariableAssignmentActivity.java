333e78dd4a6cd4c4f9c8dfe12ec877a44419a8ca
Atif <hafiz.mahmood@accenture.com>
2020-10-20 10:07:40 +0300

    activity-2

--- SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -17,7 +17,6 @@ public class VariableAssignmentActivity {
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
 		// 3- Print updated value to the console
-
 	}
 
 }
